68114914667b5299951707d87cd5594a521f66be
Silent-Aditi <imaditi005@gmail.com>
2026-07-22 11:30:25 +0530

    word to number

--- lab1/arrowfunction.js
+++ lab1/arrowfunction.js
@@ -1,18 +1,35 @@
 // write a fucntion to take number between 0 to 9 and return in words const numberToWord = (num) => {
-    const numberToWord = (num) => {
-    switch (num) {
-        case 0: return "Zero";
-        case 1: return "One";
-        case 2: return "Two";
-        case 3: return "Three";
-        case 4: return "Four";
-        case 5: return "Five";
-        case 6: return "Six";
-        case 7: return "Seven";
-        case 8: return "Eight";
-        case 9: return "Nine";
-        default: return "Invalid Number";
-    }
-};
+//     const numberToWord = (digit) => {
+//     switch (digit) {
+//         case 1: 
+//             return "One";
+//         case 2: 
+//             return "Two";
+//         case 3: 
+//             return "Three";
+//         case 4: 
+//             return "Four";
+//         case 5: 
+//             return "Five";
+//         case 6: 
+//             return "Six";
+//         case 7: 
+//             return "Seven";
+//         case 8: 
+//             return "Eight";
+//         case 9: 
+//             return "Nine";
+//         default: 
+//             return "Invalid Number";
+//     }
+// };
 
-console.log(numberToWord(5));
+// console.log(numberToWord(5));
+// console.log(numberToWord(3));
+// console.log(numberToWord(6));
+
+// create another function that takes a number and show the number in word using arrow function
+const rollNum = "2503201000072";
+const digits = String(rollNum).split("");
+    
+console.log(digits);
