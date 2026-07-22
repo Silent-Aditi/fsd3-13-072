bda62d5b11904485431f061b49c2e09cd3267c11
Silent-Aditi <imaditi005@gmail.com>
2026-07-22 11:57:11 +0530

    string

--- lab1/arrowfunction.js
+++ lab1/arrowfunction.js
@@ -1,35 +1,42 @@
-// write a fucntion to take number between 0 to 9 and return in words const numberToWord = (num) => {
-//     const numberToWord = (digit) => {
-//     switch (digit) {
-//         case 1: 
-//             return "One";
-//         case 2: 
-//             return "Two";
-//         case 3: 
-//             return "Three";
-//         case 4: 
-//             return "Four";
-//         case 5: 
-//             return "Five";
-//         case 6: 
-//             return "Six";
-//         case 7: 
-//             return "Seven";
-//         case 8: 
-//             return "Eight";
-//         case 9: 
-//             return "Nine";
-//         default: 
-//             return "Invalid Number";
-//     }
-// };
+//create another function that takes a number and that show in word 
+// with the help of two word function 
+const ToWord = (digit) => {
+   switch (digit) {
+      case 0:
+         return "Zero";
+         case 1:
+         return "One";
+         case 2:
+         return "Two";
+         case 3:
+         return "Three";
+         case 4:
+         return "Four";
+         case 5:
+         return "Five";
+         case 6:
+         return "Six";
+         case 7:
+         return "Seven";
+         case 8:
+         return "Eight";
+         case 9:
+         return "Nine";
+        default:
+            return "Invalid digit";
+    }
+};
+// const rollnum = "2503201000243";
+// const digit = rollnum.split("");
 
-// console.log(numberToWord(5));
-// console.log(numberToWord(3));
-// console.log(numberToWord(6));
-
-// create another function that takes a number and show the number in word using arrow function
-const rollNum = "2503201000072";
-const digits = String(rollNum).split("");
-    
-console.log(digits);
+// digit.forEach((d) => {
+//     console.log(ToWord(Number(d)));
+// });
+const rollnum = "2503201000243";
+const digit = String(rollnum).split("");
+console.log(digit);
+let inWord = "";
+digit.forEach((d) => {
+    inWord += " "+ ToWord(Number(d)) + " ";
+});
+console.log(inWord);
---
+++ lab1/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const rollNum = "2503201000072";
+// const digits = String(rollNum).split("");
+    
+// console.log(digits);
