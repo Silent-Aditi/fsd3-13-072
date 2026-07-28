dc31c8bb8d92feb306376b97307f806021278474
Silent-Aditi <imaditi005@gmail.com>
2026-07-28 14:00:37 +0530

    event loop

---
+++ lab1/eventloop.js
@@ -0,0 +1,20 @@
+const f1=()=>{
+   console.log("F1");
+};
+const f2=()=>{
+    console.log("F2");
+};
+const f3=()=>{
+    console.log("F3");
+};
+const main=()=>{
+    console.log("main");
+    f1(); 
+  setTimeout(f2, 0); 
+  //setInterval(f2,1000);
+  setImmediate(f3);
+  process.nextTick(f1);
+    f3();
+    console.log("end");
+};
+main();
