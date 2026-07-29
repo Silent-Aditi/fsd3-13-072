85901ca5dad0983cb717e38f406828a7e3a10f52
Silent-Aditi <imaditi005@gmail.com>
2026-07-29 12:15:22 +0530

    file 4 images

--- lab2/file3.js
+++ lab2/file3.js
@@ -1,20 +1,25 @@
-import { readFile, writeFile } from "fs/promises";
-const writeData = async (fname , contents) => {
-    await writeFile(fname,contents);
-    console.log("file written");
+import { appendFile, readFile, writeFile, unlink } from "fs/promises";
 
+const writeData = async (fname, contents) => {
+    await writeFile(fname, contents);
+    console.log("File written");
 };
 
 const readData = async (fname) => {
     const data = await readFile(fname, "utf-8");
-    console.log("file contents:");
+    console.log("File contents");
     console.log(data);
 };
+const appenddata = async(fname, contents) => {
+    await appendFile(fname, "\n" +contents);
+};
+const deletefile=async(fname)=>{
+    await fs.unlink(fname);
+    console.log('$(fname) deleted');
+}
 
-
-
-await writeData("happy.txt", "I am happy");
-await readData("happy.txt");
-await appendData ("happy.text","FSDmin interesting");
-await readData("happy.txt");
-    
+// await writeData("happy.txt", "I am very happy");
+// await readData("happy.txt");
+// await appenddata("happy.txt","fsd is interesting");
+// await readData("happy.txt");
+await deletefile("happy.txt");
