3a7aa140ca628aaf1d8771df59aa6a2b2f83a5e1
Silent-Aditi <imaditi005@gmail.com>
2026-09-22 14:02:35 +0530

    product.js

---
+++ lab3/products.js
@@ -0,0 +1,20 @@
+const products = [
+    {   
+        id:1 , 
+        name: 'Marker', 
+        qty: 100, 
+        price: 15
+    },
+    {   
+        id:2 , 
+        name: 'Duster', 
+        qty: 50, 
+        price: 20
+    },
+]
+let nextId = 3;
+
+export const getAllProducts =() =>{         //arrow function
+    return products;
+
+}
